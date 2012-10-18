0c6a45d352b09f4be5fd53c8d9d4afe2784c12be
need4spd <need4spd@naver.com>
2012-10-18 11:13:03 +0900

    change sample collection name

--- java/com/tistory/devyongsik/util/update/SampleDataMaker.java
+++ java/com/tistory/devyongsik/util/update/SampleDataMaker.java
@@ -11,21 +11,21 @@ public class SampleDataMaker {
 	public static void main(String[] args) {
 		
 		List<Map<String, String>> sampleList = new ArrayList<Map<String, String>>();
-		String field_name_1 = "board_id";
-		String field_name_2 = "title";
-		String field_name_3 = "dscr";
-		String field_name_4 = "creuser";
-		//String field_name_5 = "we_ins_user";
-		//String field_name_6 = "we_ins_date";
+		String field_name_1 = "wiki_idx";
+		String field_name_2 = "space_idx";
+		String field_name_3 = "wiki_title";
+		String field_name_4 = "wiki_text";
+		String field_name_5 = "ins_user";
+		String field_name_6 = "ins_date";
 		
 		for(int i = 0; i < 500; i++) {
 			Map<String, String> doc = new HashMap<String, String>();
 			doc.put(field_name_1, String.valueOf(i));
-			doc.put(field_name_4, "creuser"+i);
-			doc.put(field_name_3, "텍스트 입니다"+i);
-			doc.put(field_name_2, "제목 입니다"+i);
-			//doc.put(field_name_5, "need4spd");
-			//doc.put(field_name_6, "20120819");
+			doc.put(field_name_4, "본문 입니다."+i);
+			doc.put(field_name_3, "제목 입니다"+i);
+			doc.put(field_name_2, String.valueOf(i/100));
+			doc.put(field_name_5, "need4spd");
+			doc.put(field_name_6, "20120819");
 			
 			sampleList.add(doc);
 		}
