cddd531d120258eabdf82266469b32d39e2016a7
need4spd <need4spd@naver.com>
2012-12-04 12:03:25 +0900

    remove warning

--- crescent_core_web/src/main/java/com/tistory/devyongsik/controller/UpdateController.java
+++ crescent_core_web/src/main/java/com/tistory/devyongsik/controller/UpdateController.java
@@ -7,7 +7,6 @@ import java.io.Writer;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
