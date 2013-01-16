f4c9f3e1413766a9d4974eb6f3bf513cac1e0c91
need4spd <need4spd@naver.com>
2013-01-16 21:52:11 +0900

    shell file

--- crescent_utils/test_make_json_from_db.sh
+++ crescent_utils/test_make_json_from_db.sh
@@ -1,2 +1,2 @@
 cd target
-java -classpath crescent_utils-0.5-SNAPSHOT.jar:. com.tistory.devyongsik.utils.MakeJsonFormFileFromDB aws_mysql
+java -classpath crescent_utils-0.5-SNAPSHOT.jar:. com.tistory.devyongsik.utils.MakeJsonFormFileFromDB local_mysql
