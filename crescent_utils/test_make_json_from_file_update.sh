9e76688a343a22b49660c6cf11b31244f70a1e9c
need4spd <need4spd@naver.com>
2013-04-16 14:06:57 +0900

    test shell

---
+++ crescent_utils/test_make_json_from_file_update.sh
@@ -0,0 +1,2 @@
+cd target
+java -classpath crescent_utils-0.5-SNAPSHOT.jar:. com.tistory.devyongsik.utils.MakeJsonFormUpdateByFieldValueFileFromFiles /Users/need4spd/Programming/Java/workspace/crescent/crescent_utils/src/main/resources/indexing_file_info.xml
